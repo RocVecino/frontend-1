22a5f5db478cc10e82938709a7bf515497748d92
RocVecino <94741402+RocVecino@users.noreply.github.com>
2021-12-26 18:01:20 +0100

    Create Calificacion.js

---
+++ src/components/calificaciones/Calificacion.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+export default class Calificacion extends Component 
+{
+    render()
+    {
+        return (
+            <tr>
+                <td>{this.props.calificacion.Alumno}</td>
+                <td>{this.props.calificacion.Asignatura}</td>
+                <td>{this.props.calificacion.Nota}</td>
+                <td>
+                    <button className="btn btn-primary" onClick={() => this.props.onEdit(this.props.calificacion)} >Modificar</button>
+                    <button className="btn btn-primary" onClick={() => this.props.onDelete(this.props.calificacion)} >Eliminar</button>
+                </td>
+            </tr>
+        );
+    }
+}
