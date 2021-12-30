761f69a55d6630987082c9a3b7d5cf7cef23cbed
RocVecino <94741402+RocVecino@users.noreply.github.com>
2021-12-30 17:45:52 +0100

    Update CalificacionesApi.js

--- src/components/calificaciones/CalificacionesApi.js
+++ src/components/calificaciones/CalificacionesApi.js
@@ -1,22 +1,90 @@
 export default class CalificacionesApi
 {
     static API_BASE_URL = "/api/v1";
-
+    
     static requestHeaders()
     {
         return {};
     }
 
-    static getAllNotas()
+    /**METODO GET **/
+    static async getAllNotas()
     {
         const headers = CalificacionesApi.requestHeaders();
         const request = new Request(CalificacionesApi.API_BASE_URL + "/notas", {
             method: "GET",
             headers: headers
         });
+        
+        const response = await fetch(request);
 
-        return fetch(request).then(response => {
-            return response.json();
+        //capturamos el error en caso de que la repuesta no sea Ok
+        if(response.ok !== true){
+            throw Error(response.statusText);
+        }
+
+        return response.json();
+    }
+
+    /** METODO POST**/ 
+    static async addNotas(Nota)
+    {
+        const headers = CalificacionesApi.requestHeaders();
+        const request = new Request(CalificacionesApi.API_BASE_URL + "/notas", {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(Nota)
         });
+        
+        const response = await fetch(request);
+
+        //capturamos el error en caso de que la repuesta no sea Ok
+        if(response.ok !== true){
+            throw Error(response.statusText);
+        }
+
+        return response;
+    }
+    /** METODO PATCH**/ 
+    static async updateNotas(Nota)
+    {
+        const headers = CalificacionesApi.requestHeaders();
+        const request = new Request(CalificacionesApi.API_BASE_URL + "/notas", {
+            method: "PATCH",
+            headers: headers,
+            body: JSON.stringify(Nota)
+        });
+        
+        const response = await fetch(request);
+
+        console.log(response);
+
+        //capturamos el error en caso de que la repuesta no sea Ok
+        if(response.ok !== true){
+            throw Error(response.statusText);
+        }
+
+        return response;
+    }
+    /** METODO DELETE**/ 
+    static async deleteNotas(Nota)
+    {
+        const headers = CalificacionesApi.requestHeaders();
+        const request = new Request(CalificacionesApi.API_BASE_URL + "/notas", {
+            method: "DELETE",
+            headers: headers,
+            body: JSON.stringify(Nota)
+        });
+        
+        const response = await fetch(request);
+
+        console.log(response);
+
+        //capturamos el error en caso de que la repuesta no sea Ok
+        if(response.ok !== true){
+            throw Error(response.statusText);
+        }
+
+        return response;
     }
 }
